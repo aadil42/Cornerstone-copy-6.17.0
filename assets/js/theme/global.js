29043fd24da9a3cfc279eb9c30d1b4e9f0529de0
aadil42 <adilkelawala@gmail.com>
2026-02-11 15:37:00 +0530

    load react only when needed in global.js also make the menu and user modal async

--- assets/js/theme/global.js
+++ assets/js/theme/global.js
@@ -13,11 +13,6 @@ import cartPreview from './global/cart-preview';
 import carousel from './common/carousel';
 import svgInjector from './global/svg-injector';
 
-// these are react components to inject.
-import React from "react"; // this is needed here as import even though we're not using it.
-import { createRoot } from "react-dom/client";
-import TestingReactComponent from "../components/TestingReactComponent";
-import CustomUserModal from "../components/customUserModal";
 export default class Global extends PageManager {
     onReady() {
         const { cartId, 
@@ -42,36 +37,50 @@ export default class Global extends PageManager {
         this.initUserModal({settings, theme_settings, urls});
     }
 
-    initReactMobileMenu({categories, custom_categories_navigation}) {
-
+    async initReactMobileMenu({categories, custom_categories_navigation}) {
         const testingReactComponentContainer = document.querySelector("#custom-modal-id");
+        
         if (testingReactComponentContainer) {
+            const [{ default: React }, { createRoot }, { default: TestingReactComponent }] = await Promise.all([
+                import('react'),
+                import('react-dom/client'),
+                import('../components/TestingReactComponent')
+            ]);
+            
             const root = createRoot(testingReactComponentContainer);
             root.render(
-            <TestingReactComponent 
-            categories={categories} 
-            custom_categories_navigation={custom_categories_navigation}
-            menuTitle="Menu"
-            />);
+                React.createElement(TestingReactComponent, {
+                    categories,
+                    custom_categories_navigation,
+                    menuTitle: "Menu"
+                })
+            );
         }
     }
 
-    initUserModal({settings, theme_settings, urls}) {
+    async initUserModal({settings, theme_settings, urls}) {
         const userName = "Test Name";
         const userAuthModal = document.querySelector("#custom-user-auth-modal");
-        console.log('CustomUserModal', CustomUserModal);
+        
         if (userAuthModal) {
-            console.log('haha from inside', CustomUserModal);
+            const [{ default: React }, { createRoot }, { default: CustomUserModal }] = await Promise.all([
+                import('react'),
+                import('react-dom/client'),
+                import('../components/customUserModal')
+            ]);
+            
+            console.log('CustomUserModal', CustomUserModal);
             const root = createRoot(userAuthModal);
             root.render(
-            <CustomUserModal 
-            theme_settings={theme_settings}
-            settings={settings}
-            urls={urls}
-            title="This is meain title"
-            subTitle="This is just subtitle to fill the space"
-            userName={userName}
-            />);
+                React.createElement(CustomUserModal, {
+                    theme_settings,
+                    settings,
+                    urls,
+                    title: "This is meain title",
+                    subTitle: "This is just subtitle to fill the space",
+                    userName
+                })
+            );
         }        
     }
 }
